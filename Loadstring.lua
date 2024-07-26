ce332d0c2f7eb4e2003b75044c37b06d42d21e82
nunera <67439647+nunera@users.noreply.github.com>
2024-07-25 22:25:32 -0500

    Create Loadstring.lua

---
+++ Loadstring.lua
@@ -0,0 +1,14 @@
+getgenv().key = Enum.KeyCode.X
+getgenv().message = "/e death."
+
+getgenv().chatToActivate = false -- if false, it will only activate when key is pressed
+
+getgenv().teleportPlayerInsteadOfCamera = true -- if true, it will teleport player instead of camera
+getgenv().cooldown = .4
+getgenv().automatic = true
+getgenv().distance = 2
+getgenv().whitelist = {
+  
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nunera/Terminate/main/Script.lua"))()
